e1e830c16a24e98b7e6262e34e21559ae9ec8f94
Khislat <khislatolimov29@gmail.com>
2025-08-03 19:39:20 +0900

    fix: modify main.ts

--- apps/petshop-api/src/main.ts
+++ apps/petshop-api/src/main.ts
@@ -5,7 +5,7 @@ import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 import { graphqlUploadExpress } from 'graphql-upload';
 import * as express from 'express';
 import { WsAdapter } from '@nestjs/platform-ws';
-import path from 'path';
+
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -14,7 +14,7 @@ async function bootstrap() {
 	app.enableCors({ origin: true, credentials: true });
 
 	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
-	app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+	app.use('/uploads', express.static('./uploads'));
 
 	app.useWebSocketAdapter(new WsAdapter(app));
 	await app.listen(process.env.PORT_API ?? 3000);
